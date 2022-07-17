e9788574337d8cfd6b47bff7286a131b35fdefef
inuoluwadunsimi <danielolaoladeinde@gmail.com>
2022-07-17 01:54:01 +0100

    initial commit

---
+++ app.js
@@ -0,0 +1,31 @@
+const express = require('express')
+const bodyParser = require('body-parser')
+const path = require('path')
+
+
+
+
+const app = express()
+
+const users = []
+
+app.set('view engine','ejs')
+app.set('views','views')
+
+app.use(bodyParser.urlencoded({extended:false}))
+
+app.get('/',(req,res,next)=>{
+    res.render('index',{pageTitle:'Add-user'})
+})
+app.get('/users',(req,res,next)=>{
+    res.render('users',{pageTitle:'users',users:users})
+})
+app.post('/add-user',(req,res,next)=>{
+    users.push({name:req.body.username})
+    res.redirect('/users')
+})
+
+
+
+
+app.listen(3000)
